c1540fcfde2047fbabd57d7aa85a677da77c0f32
mrdocula <mrdocula@gmail.com>
2023-07-02 19:22:14 +0300

    Implement add function

--- Launcher.java
+++ Launcher.java
@@ -1,5 +1,11 @@
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a = 5;
+        int b = 3;
+        System.out.println(add(a, b));
+    }
+    private static int add(int a, int b){
+        return a + b;
     }
 }
